53eb3d3ed12e6b51502d2450b97c7ebdada67d7d
ttomi007 <samattoth@gmail.com>
2022-09-28 22:01:35 +0200

    Project init

---
+++ src/main/java/hu/innobyte/Airlines2/Airlines2Application.java
@@ -0,0 +1,13 @@
+package hu.innobyte.Airlines2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airlines2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airlines2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hu/innobyte/Airlines2/Airlines2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.innobyte.Airlines2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Airlines2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
